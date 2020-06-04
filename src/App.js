2d079920c025660d6914a46529a1c9e932b93847
nick-gibb <gibbnick@gmail.com>
2020-06-04 10:57:46 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar'
+import TypoGraphy from '@material-ui/core/Typography'
+import Posts from './posts'
+import SimpleCard from './card'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <AppBar color="primary" position="static">
+          <Toolbar>
+            <TypoGraphy
+              color="inherit"
+            >
+              Briefing Portal
+           </TypoGraphy>
+          </Toolbar>
+        </AppBar>
+        <Posts />
+        
+      </div>
   );
 }
 
---
+++ src/posts.js
@@ -0,0 +1,53 @@
+import React from "react";
+import SimpleCard from "./card";
+
+class Posts extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch("http://13.72.78.124:1337/themes")
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            items: result,
+          });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error,
+          });
+        }
+      );
+  }
+  render() {
+    const { error, isLoaded, items } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <div>
+          {items.map((item) => (
+            <SimpleCard item={item} />
+          ))}
+        </div>
+      );
+    }
+  }
+}
+
+export default Posts;
